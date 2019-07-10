93055f432c3d5284cb44aea057b051dba2a4f567
Bae Yong-Ju <b1uec0in@naver.com>
2019-07-10 12:55:09 +0900

    Remove training code

--- load_fastText.py
+++ load_fastText.py
@@ -9,8 +9,6 @@ import plac
 import numpy
 
 import spacy
-import random
-from spacy.language import Language
 
 @plac.annotations(
     model_dir=('Model output directory', 'option', 'm'))
@@ -20,17 +18,6 @@ def main(lang: 'Spacy language', vectors_loc: 'Vectors location', model_dir='mod
         python load_fastText.py en vectors/wiki.en.vec
         python load_fastText.py en vectors/wiki.en.vec -m ./model
     """
-
-TAG_MAP = {
-    'N': {'pos': 'NOUN'},
-    'V': {'pos': 'VERB'},
-    'J': {'pos': 'ADJ'}
-}
-
-TRAIN_DATA = [
-    ("Ik zie mooie dingen", {'tags': ['N', 'V', 'J', 'N']}),
-    ("Hij maakt goede muziek", {'tags': ['N','V', 'J', 'N']})
-]
 
     # create empty language class – this is required if you're planning to
     # save the model to disk and load it back later (models always need a
@@ -46,27 +33,6 @@ TRAIN_DATA = [
             word = pieces[0]
             vector = numpy.asarray([float(v) for v in pieces[1:]], dtype='f')
             nlp.vocab.set_vector(word, vector)  # add the vectors to the vocab
-
-    
-    tagger = nlp.create_pipe('tagger')
-    # Add the tags. This needs to be done before you start training.
-    for tag, values in TAG_MAP.items():
-        tagger.add_label(tag, values)
-    nlp.add_pipe(tagger)
-    optimizer = nlp.begin_training()
-    for i in range(20):
-        random.shuffle(TRAIN_DATA)
-        losses = {}
-        for text, annotations in TRAIN_DATA:
-            nlp.update([text], [annotations], sgd=optimizer, losses=losses)
-        print(losses)
-
-    # test the trained model
-    test_text = "ik wil mooie vrouwen"
-    doc = nlp(test_text)
-    print('Tags', [(t.text, t.tag_, t.pos_) for t in doc])
-
-    print("Saved model to", 'nl_model_tagger')
 
     nlp.to_disk(model_dir)
 
